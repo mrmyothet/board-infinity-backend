f7f21d37a5e657b27dfe52d2a0ed2394b5c4bed9
Myo Thet <myothet@solidplm.com>
2024-05-09 22:52:16 +0700

    Controller to View

--- coreWebApplication/Controllers/HomeController.cs
+++ coreWebApplication/Controllers/HomeController.cs
@@ -28,5 +28,10 @@ namespace coreWebApplication.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult Message()
+        {
+            return View();
+        }
     }
 }
