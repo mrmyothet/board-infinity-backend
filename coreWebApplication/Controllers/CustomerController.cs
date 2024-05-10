f16d1497f53d8a8c64a6428f4e5d0c138023036e
Myo Thet <myothet@solidplm.com>
2024-05-10 10:24:06 +0700

    Razor View Engine and Razor Syntax

---
+++ coreWebApplication/Controllers/CustomerController.cs
@@ -0,0 +1,24 @@
+﻿using coreWebApplication.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace coreWebApplication.Controllers
+{
+    public class CustomerController : Controller
+    {
+        public static readonly List<Customer> lstCustomer = new List<Customer>() 
+        { 
+            new Customer() { Id = 101, Name = "NuStar Technologies Pte Ltd", Amount = 3000 }, 
+            new Customer() { Id = 102, Name = "WEFIC - Oilfield Engineering Services", Amount = 1500 }
+        };
+
+        public IActionResult Index()
+        {
+            ViewBag.Message = "Customer Management System";
+            ViewBag.CustomerCount = lstCustomer.Count();
+            ViewBag.CustomerList = lstCustomer;
+
+            return View();
+        }
+    }
+}
