10df710b3ae7d354925a1e65ab7931946a3f3e1f
Myo Thet <myothet@solidplm.com>
2024-05-25 23:33:45 +0700

    Forms and Validation - Weakly Typed Forms

---
+++ coreWebApplication/Controllers/AccountController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace coreWebApplication.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult WeeklyTypedLogin()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult LoginPost(string userName, string password)
+        {
+            ViewBag.UserName = userName;
+            ViewBag.Password = password;
+            return View();
+        }
+    }
+}
