b96f2b574e0426e6fe3d8f41d563cb28be90cb16
Myo Thet <myothet@solidplm.com>
2024-05-25 23:59:22 +0700

    Forms and Validation - Strongly Typed Forms

--- coreWebApplication/Controllers/AccountController.cs
+++ coreWebApplication/Controllers/AccountController.cs
@@ -1,10 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using coreWebApplication.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace coreWebApplication.Controllers
 {
     public class AccountController : Controller
     {
-        public IActionResult WeeklyTypedLogin()
+        public IActionResult WeaklyTypedLogin()
         {
             return View();
         }
@@ -14,6 +15,19 @@ namespace coreWebApplication.Controllers
         {
             ViewBag.UserName = userName;
             ViewBag.Password = password;
+            return View();
+        }
+
+        public IActionResult StronglyTypedLogin()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult LoginSuccess(UserAccount item)
+        {
+            ViewBag.UserName = item.UserName;
+            ViewBag.Password = item.Password;
             return View();
         }
     }
