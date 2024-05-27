4924571def484614288f1dbf0a882460d9a3c0bf
Myo Thet <myothet@solidplm.com>
2024-05-27 15:55:13 +0700

    Module 2 - Dependency Injection - Create a new Web App (MVC)

---
+++ Module2.CoreWebMVCApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Module2.CoreWebMVCApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
