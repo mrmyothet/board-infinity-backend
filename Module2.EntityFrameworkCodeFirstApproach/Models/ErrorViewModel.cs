b6b19e341da0bdd874c7f46cdbc849cb8add5542
Myo Thet <myothet@solidplm.com>
2024-05-27 23:52:43 +0700

    Module 2 - EFCore - Code First Approach - Add New Project

---
+++ Module2.EntityFrameworkCodeFirstApproach/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Module2.EntityFrameworkCodeFirstApproach.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
