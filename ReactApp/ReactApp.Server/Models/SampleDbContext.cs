ba65b9348dabc33e2d02bc8c89a2099dd4c95213
Myo Thet <mr.myothet@gmail.com>
2024-06-23 01:01:24 +0700

    Project - Add Employees API Controller

---
+++ ReactApp/ReactApp.Server/Models/SampleDbContext.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace ReactApp.Server.Models;
+
+public partial class SampleDbContext : DbContext
+{
+    public SampleDbContext()
+    {
+    }
+
+    public SampleDbContext(DbContextOptions<SampleDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Employee> Employees { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=MYOTHETPC\\MSSQLSERVER2012; Database=SampleDB; Integrated Security=True; TrustServerCertificate=True; User Id=sa; Password=admin123!");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Employee>(entity =>
+        {
+            entity.HasKey(e => e.EmployeeId).HasName("PK__Employee__7AD04FF1AB3193BE");
+
+            entity.Property(e => e.EmployeeId).HasColumnName("EmployeeID");
+            entity.Property(e => e.City)
+                .HasMaxLength(20)
+                .IsUnicode(false);
+            entity.Property(e => e.Department)
+                .HasMaxLength(20)
+                .IsUnicode(false);
+            entity.Property(e => e.Gender)
+                .HasMaxLength(6)
+                .IsUnicode(false);
+            entity.Property(e => e.Name)
+                .HasMaxLength(20)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
