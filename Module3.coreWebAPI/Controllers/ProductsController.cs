6a8928960659aa9c220cbb0d5c49c6cb613e69cd
Myo Thet <mr.myothet@gmail.com>
2024-06-22 17:18:06 +0700

    Module 3 - Get, Post, Put API on product model

---
+++ Module3.coreWebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Module3.coreWebAPI.Models;
+
+namespace Module3.coreWebAPI.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ProductsController : ControllerBase
+{
+    private readonly ApplicationDbContext _context;
+
+    public ProductsController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public IEnumerable<Product> GetProducts()
+    {
+        return _context.Products.ToList();
+    }
+
+    [HttpPost]
+    public IActionResult AddProduct(Product product)
+    {
+        try
+        {
+            _context.Products.Add(product);
+            _context.SaveChanges();
+
+            return StatusCode(StatusCodes.Status201Created, product);
+        }
+        catch (Exception)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError);
+        }
+    }
+
+    [HttpGet("{id}")]
+    public Product Get(int id)
+    {
+        return _context.Products.Find(id);
+    }
+
+    [HttpPut("{id}")]
+    public IActionResult UpdateProduct(int id, Product product)
+    {
+        try
+        {
+            if (id != product.ProductId)
+            {
+                return StatusCode(StatusCodes.Status400BadRequest);
+            }
+
+            _context.Products.Update(product);
+            _context.SaveChanges(true);
+
+            return StatusCode(StatusCodes.Status200OK);
+        }
+        catch (Exception)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError);
+        }
+    }
+}
