63f019cdef5f9a3a0237e9b57b7633a031b5bf21
Myo Thet <mr.myothet@gmail.com>
2024-06-22 18:04:54 +0700

    Module 3 - Consume Web Api from MVC project - Get products

---
+++ Module3.Module3.ConsumeWebAPI/Controllers/ProductController.cs
@@ -0,0 +1,103 @@
+﻿using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Module3.Module3.ConsumeWebAPI.Models;
+using Newtonsoft.Json;
+
+namespace Module3.Module3.ConsumeWebAPI.Controllers;
+
+public class ProductController : Controller
+{
+    private readonly string baseUrl = "https://localhost:7006/";
+
+    // GET: ProductController
+    public async Task<ActionResult> Index()
+    {
+        List<Product> products = new List<Product>();
+
+        string productsUrl = baseUrl + "api/products";
+
+        using (var client = new HttpClient())
+        {
+            var response = await client.GetAsync(productsUrl);
+
+            string jsonResponse = await response.Content.ReadAsStringAsync();
+            if (jsonResponse is null)
+                return BadRequest(jsonResponse);
+
+            products = JsonConvert.DeserializeObject<List<Product>>(jsonResponse)!;
+        }
+
+        return View(products);
+    }
+
+    // GET: ProductController/Details/5
+    public ActionResult Details(int id)
+    {
+        return View();
+    }
+
+    // GET: ProductController/Create
+    public ActionResult Create()
+    {
+        return View();
+    }
+
+    // POST: ProductController/Create
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public ActionResult Create(IFormCollection collection)
+    {
+        try
+        {
+            return RedirectToAction(nameof(Index));
+        }
+        catch
+        {
+            return View();
+        }
+    }
+
+    // GET: ProductController/Edit/5
+    public ActionResult Edit(int id)
+    {
+        return View();
+    }
+
+    // POST: ProductController/Edit/5
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public ActionResult Edit(int id, IFormCollection collection)
+    {
+        try
+        {
+            return RedirectToAction(nameof(Index));
+        }
+        catch
+        {
+            return View();
+        }
+    }
+
+    // GET: ProductController/Delete/5
+    public ActionResult Delete(int id)
+    {
+        return View();
+    }
+
+    // POST: ProductController/Delete/5
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public ActionResult Delete(int id, IFormCollection collection)
+    {
+        try
+        {
+            return RedirectToAction(nameof(Index));
+        }
+        catch
+        {
+            return View();
+        }
+    }
+}
---
+++ Module3.Module3.ConsumeWebAPI/Models/Product.cs
@@ -0,0 +1,8 @@
+﻿namespace Module3.Module3.ConsumeWebAPI.Models;
+
+public class Product
+{
+    public int ProductId { get; set; }
+    public string? Name { get; set; }
+    public double Price { get; set; }
+}
