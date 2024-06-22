1896a8117a53c67b5aad8d649c9dfd4a72faf9f7
Myo Thet <mr.myothet@gmail.com>
2024-06-22 17:23:22 +0700

    Module 3 - Add New MVC Project - to consume Web API

---
+++ Module3.Module3.ConsumeWebAPI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Module3.Module3.ConsumeWebAPI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
